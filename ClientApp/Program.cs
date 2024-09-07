1ee012f1eeb51ba8d7aeb8d19e9b79e72c56d75d
Hakim <ghods.hakim@hotmail.com>
2024-09-07 12:54:16 +0330

    clean-up

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -1,18 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-//for (int i = 0; i < 100; i++)
-//{
-//    using var client = new HttpClient(); // Creates new HttpClient each time
-//    await client.GetAsync("https://codehaks.com");
-//    await Task.Delay(1000);
-//    Console.WriteLine($"{i} - Connection established");
-//}
+﻿Console.WriteLine("Hello, World!");
 
 for (int i = 0; i < 100; i++)
 {
-    var client = new HttpClient(); // Creates new HttpClient each time
+    using var client = new HttpClient(); // Creates new HttpClient each time
     await client.GetAsync("https://codehaks.com");
     await Task.Delay(1000);
     Console.WriteLine($"{i} - Connection established");
