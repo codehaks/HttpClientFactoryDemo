003261cab3e2f47af27d6a8c384880a89d254587
Hakim <ghods.hakim@hotmail.com>
2024-09-07 10:43:15 +0330

    client app

---
+++ OldClientApp/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World!");
+
+for (int i = 0; i < 10; i++)
+{
+    var client = new HttpClient();
+    await client.GetAsync("https://codehaks.com");
+    await Task.Delay(1000);
+    Console.WriteLine($"{i} - Connection established");
+}
